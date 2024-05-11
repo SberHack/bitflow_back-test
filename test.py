a816aedd3516a4d425a21bf7795da30d1e0aaeca
Andrew Alexeev <66908991+Azaki-san@users.noreply.github.com>
2024-05-11 14:51:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 from p1337x import py1337x
 
-a = requests.get("http://www.1337x.to")
+a = requests.get("http://www.1337x.st")
 print(a)
-search_engine = py1337x(proxy='1337x.to', cache='py1337xCache', cacheTime=500)
+search_engine = py1337x(proxy='1337x.st', cache='py1337xCache', cacheTime=500)
 print(search_engine.search("gta"))
